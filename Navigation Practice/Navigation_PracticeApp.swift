4b1b8d158dfa3d3c9821a624faa4c2a1dbfbfc77
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-17 13:01:14 -0400

    Initial Commit

---
+++ Navigation Practice/Navigation_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_PracticeApp.swift
+//  Navigation Practice
+//
+//  Created by Jessica Cooper on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
